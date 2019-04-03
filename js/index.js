b2d32c3a64e2ab9f32485fd3e764f5e439b5e40d
JuliandroR <juliandro.ribeiro@novaandradina.org>
2019-04-02 22:15:16 -0400

    Detalhes do esqueleto da página

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 jQuery(function () {
   jQuery(window).scroll(function () {
     if (jQuery(this).scrollTop() > 10) {
-      $("#header").css("background", "#D7CEC7");
+      $("#header").css("background", "rgba(215, 206, 199, 0.8)");
       $("#header").css("color", "#233237");
     } else {
       $("#header").css("background", "#233237");
